749999ea42f495aec99c175b8132acb896df1441
Yogi Dewanto <yogi.dewanto.ft22@mail.umy.ac.id>
2023-05-25 11:43:50 +0700

    fungsi double linkedlist

--- Double linked list_023/Double linked list_023.cpp
+++ Double linked list_023/Double linked list_023.cpp
@@ -113,3 +113,19 @@ void DoubleLinkedList::ascending() {
 		}
 	}
 }
+
+void DoubleLinkedList::descending() {
+	if (listEmpty())
+		cout << "\nList is empty" << endl;
+	else {
+		cout << "\nRecords in descending order of roll number are:" << endl;
+		Node* currentNode = START;
+		while (currentNode->next != NULL)
+			currentNode = currentNode->next;
+
+		while (currentNode != NULL) {
+			cout << currentNode->noMhs << " " << currentNode->name << endl;
+			currentNode = currentNode->prev;
+		}
+	}
+}
