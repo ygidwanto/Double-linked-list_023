9b0ebbf8515780909dc7fd4a6b016e79fd1f8763
Yogi Dewanto <yogi.dewanto.ft22@mail.umy.ac.id>
2023-05-25 10:28:55 +0700

    memasukan fungsi double linked list

--- Double linked list_023/Double linked list_023.cpp
+++ Double linked list_023/Double linked list_023.cpp
@@ -9,3 +9,19 @@ public:
 	Node* next;
 	Node* prev;
 };
+
+class DoubleLinkedList {
+private:
+	Node* START;
+public:
+
+	DoubleLinkedList();
+	void addNode();
+	bool search(int rollNo, Node** previous, Node** current);
+	bool deleteNode(int rollNo);
+	bool listEmpty();
+	void ascending();
+	void descending();
+	void hapus();
+	void searchData();
+};
