b6ec92b51e82ad315f654a674820c91edf610661
Yogi Dewanto <yogi.dewanto.ft22@mail.umy.ac.id>
2023-05-25 10:53:33 +0700

    membuat new node

--- Double linked list_023/Double linked list_023.cpp
+++ Double linked list_023/Double linked list_023.cpp
@@ -41,4 +41,16 @@ void DoubleLinkedList::addNode() {
 	Node* newNode = new Node();
 	newNode->noMhs = nim;
 	newNode->name = nm;
+
+	if (START == NULL || nim <= START->noMhs) {
+		if (START != NULL && nim == START->noMhs) {
+			cout << "\nDuplicate number not allowed" << endl;
+			return;
+		}
+		newNode->next = START;
+		if (START != NULL)
+			START->prev = NULL;
+		START = newNode;
+		return;
+	}
 }
